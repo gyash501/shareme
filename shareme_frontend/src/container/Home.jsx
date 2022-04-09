30beb234dc6cdfe4e6c5de89f1b13b80875277c5
gyash501 <gyash501@gmail.com>
2022-04-09 21:37:26 +0530

    init

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -8,15 +8,13 @@ import { userQuery } from "../utils/data";
 import { client } from "../client";
 import Pins from "./Pins";
 import logo from "../assets/logo.png";
+import { fetchuser } from "../utils/fetchUser";
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState();
   const scrollRef = useRef(null);
-  const userInfo =
-    localStorage.getItem("user") !== "undefined"
-      ? JSON.parse(localStorage.getItem("user"))
-      : localStorage.clear();
+  const userInfo =fetchuser();
 
   useEffect(() => {
     const query = userQuery(userInfo?.googleId);
